5cb178c844c763ddae4197e7abee9c7b999117d9
demoks <demokstar@gmail.com>
2014-11-11 16:29:47 +0600

    update

--- modx/assets/snippets/payment/config/payeer.php
+++ modx/assets/snippets/payment/config/payeer.php
@@ -1,12 +1,12 @@
 <?php
 
-define('PAYEER_MERCHANT_URL', '//payeer.com/merchant/');
-define('PAYEER_MERCHANT_ID', '31480869');
-define('PAYEER_SECRET_KEY', '1234567');
-define('PAYEER_ORDER_DESC', 'Клевая покупка!');
-define('PAYEER_CURRENCY_CODE', 'RUB');
-define('PAYEER_IPFILTER', '123.456.78.90');
-define('PAYEER_EMAILERR', 'demokstar@gmail.com');
-define('PAYEER_LOGFILE', '/payeer_orders.log');
+define('PAYEER_MERCHANT_URL', '//payeer.com/merchant/');// путь для оплаты, не менять без уведомления
+define('PAYEER_MERCHANT_ID', '');						// идентификатор магазина
+define('PAYEER_SECRET_KEY', '');						// секретный ключ
+define('PAYEER_ORDER_DESC', '');						// комментарий от сайта
+define('PAYEER_CURRENCY_CODE', '');						// валюта магазина (RUB,EUR, USD)
+define('PAYEER_IPFILTER', '');							// ip фильтр, доверенные ip обработчика через запятую (можно указать маску)
+define('PAYEER_EMAILERR', '');							// email для ошибок оплаты
+define('PAYEER_LOGFILE', '');							// путь до файла-журнала, где идет запись оплат заказов
 
 ?>
