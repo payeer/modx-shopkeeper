020a133d657df92af000a49b57a2980f697e6896
demoks <demokstar@gmail.com>
2014-11-11 16:05:54 +0600

    update

--- modx/assets/snippets/payment/config/payeer.php
+++ modx/assets/snippets/payment/config/payeer.php
@@ -1,11 +1,12 @@
 <?php
 
 define('PAYEER_MERCHANT_URL', '//payeer.com/merchant/');
-define('PAYEER_MERCHANT_ID', '');
-define('PAYEER_SECRET_KEY', '');
+define('PAYEER_MERCHANT_ID', '31480869');
+define('PAYEER_SECRET_KEY', '1234567');
+define('PAYEER_ORDER_DESC', 'Клевая покупка!');
 define('PAYEER_CURRENCY_CODE', 'RUB');
-define('PAYEER_IPFILTER', '');
-define('PAYEER_EMAILERR', '');
-define('PAYEER_LOGFILE', '1');
+define('PAYEER_IPFILTER', '123.456.78.90');
+define('PAYEER_EMAILERR', 'demokstar@gmail.com');
+define('PAYEER_LOGFILE', '/payeer_orders.log');
 
 ?>
