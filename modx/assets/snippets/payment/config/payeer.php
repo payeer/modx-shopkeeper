bfe64b553554a2f9975aa42e62e6ece3d45ce380
dldemoks <demokstar@gmail.com>
2016-03-17 15:59:12 +0700

    update

--- modx/assets/snippets/payment/config/payeer.php
+++ modx/assets/snippets/payment/config/payeer.php
@@ -1,12 +1,10 @@
 <?php
-
-define('PAYEER_MERCHANT_URL', '//payeer.com/merchant/');// путь для оплаты, не менять без уведомления
-define('PAYEER_MERCHANT_ID', '');						// идентификатор магазина
-define('PAYEER_SECRET_KEY', '');						// секретный ключ
-define('PAYEER_ORDER_DESC', '');						// комментарий от сайта
-define('PAYEER_CURRENCY_CODE', '');						// валюта магазина (RUB,EUR, USD)
-define('PAYEER_IPFILTER', '');							// ip фильтр, доверенные ip обработчика через запятую (можно указать маску)
-define('PAYEER_EMAILERR', '');							// email для ошибок оплаты
-define('PAYEER_LOGFILE', '');							// путь до файла-журнала, где идет запись оплат заказов
-
+	define('PAYEER_MERCHANT_URL', 'https://payeer.com/merchant/'); // путь для оплаты, не менять без уведомления
+	define('PAYEER_MERCHANT_ID', ''); // идентификатор магазина
+	define('PAYEER_SECRET_KEY', ''); // секретный ключ
+	define('PAYEER_ORDER_DESC', ''); // комментарий от сайта
+	define('PAYEER_CURRENCY_CODE', ''); // валюта магазина (RUB, EUR или USD)
+	define('PAYEER_IPFILTER', ''); // ip фильтр, доверенные ip обработчика через запятую (можно указать маску)
+	define('PAYEER_EMAILERR', ''); // email для ошибок оплаты
+	define('PAYEER_LOGFILE', ''); // путь до журнала операций оплаты (например, /payeer_orders.log)
 ?>
--- modx/assets/snippets/payment/payeer.inc.php
+++ modx/assets/snippets/payment/payeer.inc.php
@@ -1,30 +1,22 @@
 <?php
-
-require_once MODX_BASE_PATH."assets/snippets/payment/config/payeer.php";
+require_once MODX_BASE_PATH . "assets/snippets/payment/config/payeer.php";
 
 switch ($_GET['m_status'])
 {
     case 'fail':
 		$output = '<br/> <h1>Ваш заказ был отменен.</h1>';
-		
         return $output;
-		
         break;
 		
     case 'success':
 		$output = '<br/><h1>Ваш заказ успешно оплачен!</h1>';
-		
         return $output;
-		
         break;
 		
     default:
 		$m_shop = PAYEER_MERCHANT_ID;
-		
 		$dbprefix = $modx->db->config['table_prefix'];
-		
 		$mod_table = $dbprefix . "manager_shopkeeper";
-		
 		$m_orderid = $modx->db->getValue($modx->db->select("id", $mod_table, "", "id desc limit 1", ""));
 		
 		if (!$m_orderid)
@@ -33,17 +25,11 @@ switch ($_GET['m_status'])
 		}
 		
 		$amount = $modx->db->getValue($modx->db->select("price", $mod_table, "id = $m_orderid", "", ""));
-		
 		$m_amount = number_format($amount, 2, '.', '');
-			
 		$change_status = $modx->db->update(array('status' => 2), $mod_table, "id = $m_orderid");
-		
 		$modx->invokeEvent('OnSHKChangeStatus', array('order_id'=>$m_orderid, 'status' => 2));
-
 		$m_desc = base64_encode(PAYEER_ORDER_DESC);
-		
-		$m_curr = PAYEER_CURRENCY_CODE;
-		
+		$m_curr = PAYEER_CURRENCY_CODE == 'RUR' ? 'RUB' : PAYEER_CURRENCY_CODE;
 		$m_key = PAYEER_SECRET_KEY;
 
 		$arHash = array(
@@ -54,6 +40,7 @@ switch ($_GET['m_status'])
 			$m_desc,
 			$m_key
 		);
+		
 		$sign = strtoupper(hash('sha256', implode(':', $arHash)));
 		
 		$output  = '
@@ -66,7 +53,7 @@ switch ($_GET['m_status'])
 		<input type="hidden" name="m_curr" value="' . $m_curr . '">
 		<input type="hidden" name="m_desc" value="' . $m_desc . '">
 		<input type="hidden" name="m_sign" value="' . $sign . '">
-		<input type="submit" name="m_process" value="Оплатить через Payeer"><br/><br/>';
+		<input type="submit" name="m_process" value="Оплатить"><br/><br/>';
 		
 		return $output;
     break;
